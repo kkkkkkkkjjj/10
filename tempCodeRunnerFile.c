abf8beb0c7277dab738e5c152680833dbfc72842
kkkkkkkkjjj <cay351@sookmyung.ac.kr>
2023-11-09 15:32:58 +0900

    third

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     char str[4];
     str[0] = 'a';
     str[1] = 'b';
-    str[2] = ‘c’; 
-    str[3] = ‘\0’; 
+    str[2] = 'c'; 
+    str[3] = '\0'; 
 
     printf("%s\n", str);
     while (str[i]!= '\0'){
